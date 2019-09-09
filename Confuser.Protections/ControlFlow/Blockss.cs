034ef7deb2eff3ea8d5a4c1c15f38b16a66f7efa
Rzy <45715088+Riziebtw@users.noreply.github.com>
2019-09-09 20:24:57 +0200

    Add files via upload

---
+++ Confuser.Protections/ControlFlow/Blockss.cs
@@ -0,0 +1,30 @@
+﻿using Core.Helper;
+using Core.Helper.Generator.Context;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confuser.Protections
+{
+    public class Blockss
+    {
+        public List<Block> blocks = new List<Block>();
+        private Generator generator = new Generator();
+        public Block getBlock(int id)
+        {
+            return blocks.Single(block => block.ID == id);
+        }
+
+        public void Scramble(out Blockss incGroups)
+        {
+            Blockss groups = new Blockss();
+            foreach (var group in blocks)
+                groups.blocks.Insert(generator.Generate<int>(GeneratorType.Integer, groups.blocks.Count), group);
+            incGroups = groups;
+        }
+
+    }
+}
